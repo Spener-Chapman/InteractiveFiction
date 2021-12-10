e3d93bcb9d990d501d12bbe8af38989cd1179853
Spener-Chapman <spencerchap@hotmail.com>
2021-12-10 11:08:15 -0400

    finished error checking

--- InteractiveFiction/Program.cs
+++ InteractiveFiction/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace InteractiveFiction
 {
@@ -17,6 +18,7 @@ namespace InteractiveFiction
     class Program
     {
         static bool gameOver = false;
+        
         static bool OnTitleScreen = true;
 
 
@@ -25,7 +27,7 @@ namespace InteractiveFiction
         static int currentPage = 0;
 
         static string[] page;
-        static string[] savePage;
+        static string savePage;
         static string storyWritten;
         static string optionA;
         static string optionB;
@@ -37,11 +39,16 @@ namespace InteractiveFiction
 
         static void Main(string[] args)
         {
-            story = System.IO.File.ReadAllLines(@"story.txt");
-            savePage = System.IO.File.ReadAllLines(@"savefile.txt");
-            WriteTitlePage(); // since it's initialized to be the title page, this will always play the title page here
+            
+            BootUp();       // checks for errors and boots up the story file, which is needed during the error check
+
+            
+            
             
 
+            WriteTitlePage(); // since it's initialized to be the title page, this will always play the title page here
+
+            Console.Write(savePage);
             while (gameOver == false)
             {
 
@@ -51,22 +58,24 @@ namespace InteractiveFiction
                 {
                     if (readKeyInput.Key == ConsoleKey.A)             // removes this ability and brings you to page one
                     {
+                        
                         currentPage = Int32.Parse(pageA);
                         OnTitleScreen = false;                      
                         PageChange();
                     }
                     else if (readKeyInput.Key == ConsoleKey.B)  // finds the save file, and writes what page was saved on it
                     {
-                        if (string.IsNullOrWhiteSpace(savePage[0]))
+                        if (System.IO.File.Exists(@"savefile.txt"))
                         {
-                            Console.WriteLine("There is no save found");
+                            savePage = System.IO.File.ReadAllText(@"savefile.txt");
+                            page = savePage.Split('_');
+                            
+                            WritePage();
+                            OnTitleScreen = false;
                         }
                         else
                         {
-                            
-                            page = savePage;
-                            WritePage();
-                            OnTitleScreen = false;
+                            Console.WriteLine("There is no save found");         // errpr checking for no save
                         }
                     }
                     else if (readKeyInput.Key == ConsoleKey.C)        // quits the game
@@ -85,8 +94,10 @@ namespace InteractiveFiction
                         }
                         else
                         {
+                            
                             currentPage = Int32.Parse(pageA);
-                            PageChange();
+                            TurnPage();
+                            
                         }
                     }
                     else if (readKeyInput.Key == ConsoleKey.B)        // chooses option b
@@ -97,14 +108,19 @@ namespace InteractiveFiction
                         }
                         else
                         {
+                            
                             currentPage = Int32.Parse(pageB);
-                            PageChange();
+                            TurnPage();
+                            
                         }
                     }
                     else if (readKeyInput.Key == ConsoleKey.C)        // saves what page you're on in a file called savefile.txt
                     {
-                        System.IO.File.WriteAllLines(@"savefile.txt", page);
-                        savePage = System.IO.File.ReadAllLines(@"savefile.txt");
+                        SaveGame();
+                        
+                    }
+                    else if (readKeyInput.Key == ConsoleKey.D)
+                    {
                         gameOver = true;
                     }
                 }
@@ -112,6 +128,113 @@ namespace InteractiveFiction
 
         }
         
+        static void BootUp()
+        {
+            FindStory();
+            FindBlankStory();
+            story = System.IO.File.ReadAllLines(@"story.txt");
+            FindBlankLines();
+        }
+
+        static void FindStory() // checks if the story file exists
+        {
+            if (System.IO.File.Exists(@"story.txt"))
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("There is no story file");
+                Console.WriteLine("");
+                Console.WriteLine("Press any Key to exit");
+                Console.WriteLine("");
+                Console.ReadKey(true);
+                Environment.Exit(0);
+            }
+        }
+
+        static void FindBlankStory()
+        {
+            if (string.IsNullOrWhiteSpace(System.IO.File.ReadAllText(@"story.txt")))
+            {
+                Console.WriteLine("Story.txt is empty");
+                Console.WriteLine("Press any Key to exit");
+                Console.WriteLine("");
+                Console.ReadKey(true);
+                Environment.Exit(0);
+            }
+            else 
+            {
+                
+            }
+        }
+
+        static void FindBlankLines()
+        {
+            for (int i = 0; i <= 16; i++)
+            {
+                if (string.IsNullOrEmpty(story[i]))
+                {
+                    Console.WriteLine("There is a missing line of story");
+                    Console.WriteLine("");
+                    Console.WriteLine("Press any Key to exit");
+                    Console.WriteLine("");
+                    Console.ReadKey(true);
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+        static void CheckDelimiterLimit()
+        {
+            if (page.Length > 5)
+            {
+                Console.WriteLine("there are too many delimiters in this string");
+                Console.WriteLine("");
+                Console.WriteLine("Press any Key to exit");
+                Console.WriteLine("");
+                Console.ReadKey(true);
+                Environment.Exit(0);
+            }
+            if (page.Length < 5)
+            {
+                Console.WriteLine("there are too few delimiters in this string");
+                Console.WriteLine("");
+                Console.WriteLine("Press any Key to exit");
+                Console.WriteLine("");
+                Console.ReadKey(true);
+                Environment.Exit(0);
+            }
+        }
+        static void TurnPage()
+        {
+            if (currentPage > 16)
+            {
+                Console.WriteLine("There is no page found with this number.");
+                Console.WriteLine("");
+                Console.WriteLine("Going back to title page.");
+                Console.WriteLine("");
+                Console.ReadKey(true);
+                ReturnToTitle();
+            }
+            else if (currentPage < 0)
+            {
+                Console.WriteLine("There is no page found with this number.");
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to return to the title page.");
+                Console.WriteLine("");
+                Console.ReadKey(true);
+                ReturnToTitle();
+            }
+            else
+            {
+                PageChange();
+            }
+        }
         static void WriteTitlePage()
         {
             page = story[0].Split('_');
@@ -135,7 +258,16 @@ namespace InteractiveFiction
             Console.WriteLine("Quit Game");
 
         }
-       
+
+        static void SaveGame()
+        {
+            page = story[currentPage].Split('_');
+            System.IO.File.WriteAllText(@"savefile.txt", story[currentPage]);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Game Saved");
+            Console.ResetColor();
+            
+        }
         static void WritePage()
         {
             storyWritten = page[0];
@@ -147,35 +279,37 @@ namespace InteractiveFiction
             Console.WriteLine("");
             Console.WriteLine(storyWritten);
             Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("What will you do?");
-            Console.ResetColor();
+            
             if (string.IsNullOrWhiteSpace(page[3]))
-            {
-                
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("A ");
-                Console.ResetColor();
-                Console.WriteLine(optionA);
-            }
-            if (string.IsNullOrWhiteSpace(page[4]))
             {
 
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("What will you do?");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("A ");
+                Console.ResetColor();
+                Console.WriteLine(optionA);
+
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("B ");
                 Console.ResetColor();
                 Console.WriteLine(optionB);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("C ");
+                Console.ResetColor();
+                Console.WriteLine("Save");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("D ");
+                Console.ResetColor();
+                Console.WriteLine("Quit");
             }
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("C ");
-            Console.ResetColor();
-            Console.WriteLine("Save & Quit");
         }
 
         static void ReturnToTitle()
@@ -189,6 +323,7 @@ namespace InteractiveFiction
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
+            OnTitleScreen = true;
         }
         
         
@@ -197,8 +332,8 @@ namespace InteractiveFiction
         {
             
             page = story[currentPage].Split('_');
+            CheckDelimiterLimit();
 
-            
 
             WritePage();
 
